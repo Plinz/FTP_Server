7f5f0d053aaa3cea26355b2131cfed7c3c42fe7e
Cieldara <benjamin.gontard2912@gmail.com>
2017-04-04 13:43:29 +0200

    Begin commands

--- FTPclient.c
+++ FTPclient.c
@@ -6,46 +6,48 @@
 #include <time.h>
 #include <libgen.h>
 
-int main(int argc, char **argv)
-{
-    int clientfd, transfered;
-    size_t n;
-    char *host, *token, buf[MAXLINE], filename[MAXLINE];
-    rio_t rio;
-    double duration, vitesse;
-    time_t start, stop;
+/* Affiche les informations à propos du transfert de fichier */
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <host>\n", argv[0]);
-        exit(0);
-    }
-    host = argv[1];
+void handleLS(){
 
-    clientfd = Open_clientfd(host, 2121);
-    transfered = 0;
 
-    struct sockaddr_in clientaddr;
-    socklen_t clientlen = (socklen_t)sizeof(clientaddr);
-    getsockname(clientfd, (struct sockaddr *)&clientaddr, &clientlen);
-    printf("client connected to server OS port : %d, addr : %s\n", ntohs(clientaddr.sin_port), inet_ntoa(clientaddr.sin_addr));
 
-    Rio_readinitb(&rio, clientfd);
+}
 
-    if (Fgets(buf, MAXLINE, stdin) != NULL) {
 
-        memcpy(filename, buf, strlen(buf)-1);
+void printInfo(time_t start,time_t stop,int transfered){
+
+	double duration = difftime(stop, start);
+        double vitesse = (transfered/1000)/duration;
+        if (duration == 0)
+        	printf("Fin du transfert de fichier : %d octets transférés. Vitesse trop rapide pour être calculée\n",transfered);
+        else
+        	printf("Fin du transfert de fichier : %d octets transférés en %f secondes, vitesse :%f Kb/s\n",transfered, duration, vitesse);
+
+}
+
+/* Gestion du transfert de fichier */
+void handleFileTransfer(char * buf,int clientfd,rio_t rio){
+
+	char * token,filename[MAXLINE];
+	int transfered;
+    	time_t start, stop;
+	size_t n;
+
+	memcpy(filename, buf, strlen(buf)-1);
         Rio_writen(clientfd, buf, strlen(buf));
+	transfered = 0;
 
         if ((n = Rio_readlineb(&rio, buf, 3)) != 0) {
 
-            if (!strcmp(buf,"OK")){
-                printf("toto:%s",buf);
+            if (strcmp(buf,"OK") == 0){
+                printf("toto:%s\n",buf);
             } else {
-                printf("tata:%s",buf);
+                printf("tata:%s\n",buf);
             }
 
             token = strtok(buf, "-");
-            if (!strcmp(token, "OK")){
+            if (strcmp(token, "OK") == 0){
                 while( token != NULL ) {
                     printf( "%s\n", token );
                     token = strtok(NULL, "-");
@@ -61,12 +63,8 @@ int main(int argc, char **argv)
                 }
             	fclose(fp);
                 time(&stop);
-                duration = difftime(stop, start);
-                vitesse = (transfered/1000)/duration;
-                if (duration == 0)
-                    printf("Fin du transfert de fichier : %d octets transférés. Vitesse trop rapide pour être calculé\n",transfered);
-                else
-                    printf("Fin du transfert de fichier : %d octets transférés en %f secondes, vitesse :%f Kb/s\n",transfered, duration, vitesse);
+
+                printInfo(start,stop, transfered);
 
             } else {
                 while( token != NULL ) {
@@ -76,6 +74,44 @@ int main(int argc, char **argv)
                 }
             }
         }
+}
+
+int main(int argc, char **argv)
+{
+    int clientfd;
+    
+    char *host, buf[MAXLINE], *text;
+    rio_t rio;
+
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <host>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+
+    clientfd = Open_clientfd(host, 2121);
+   
+
+    struct sockaddr_in clientaddr;
+    socklen_t clientlen = (socklen_t)sizeof(clientaddr);
+    getsockname(clientfd, (struct sockaddr *)&clientaddr, &clientlen);
+    printf("client connected to server OS port : %d, addr : %s\n", ntohs(clientaddr.sin_port), inet_ntoa(clientaddr.sin_addr));
+
+    Rio_readinitb(&rio, clientfd);
+
+    if (Fgets(buf, MAXLINE, stdin) != NULL) {
+	
+	text = strtok(buf, " ");
+	printf("Test ;;;;;;;;  %s et %s\n",text);
+	if(strcmp(text,"GET")==0){
+		text = strtok(NULL, " ");;
+		printf("BUF size : %ld : text : %s \n",strlen(text),text);
+		handleFileTransfer(text,clientfd,rio);
+	}
+	else if(strcmp(text,"LS")==0)
+		printf("BUF size : %ld : text : %s \n",strlen(text),text);
+		
     }
     Close(clientfd);
     exit(0);
